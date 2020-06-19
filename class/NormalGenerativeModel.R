0c939f5f6132bdd6d9f2eb220bc6226efe318aa0
yoshiki301 <yoshiki301@gmail.com>
2020-06-20 05:49:58 +0900

    [add] normal generative model

---
+++ class/NormalGenerativeModel.R
@@ -0,0 +1,105 @@
+library("R6")
+library("assertthat")
+
+NormalGenerativeModel <- R6Class("NormalGenerativeModel",
+  public = list(
+    mu1 = NULL,
+    mu2 = NULL,
+    sigma1 = NULL,
+    sigma2 = NULL,
+    initialize = function(b1, b2, b3, b4, b5, b6) {
+      # check assertion and set private member
+      assert_that(
+        is.numeric(b1),
+        is.numeric(b2),
+        is.numeric(b3),
+        is.numeric(b4),
+        is.numeric(b5),
+        is.numeric(b6),
+        length(b1) == 2,
+        length(b2) == 2,
+        length(b3) == 2,
+        length(b4) == 3,
+        length(b5) == 2,
+        length(b6) == 3
+      )
+      private$m.b1 <<- b1
+      names(private$m.b1) <<- c("b01", "b11")
+      private$m.b2 <<- b2
+      names(private$m.b2) <<- c("b02", "b12")
+      private$m.b3 <<- b3
+      names(private$m.b3) <<- c("b03", "b13")
+      private$m.b4 <<- b4
+      names(private$m.b4) <<- c("b04", "b14", "b24")
+      private$m.b5 <<- b5
+      names(private$m.b5) <<- c("b05", "b15")
+      private$m.b6 <<- b6
+      names(private$m.b6) <<- c("b06", "b16", "b26")
+    },
+    printPrameters = function() {
+      # print private members as parameters
+      print(private$m.b1)
+      print(private$m.b2)
+      print(private$m.b3)
+      print(private$m.b4)
+      print(private$m.b5)
+      print(private$m.b6)
+    },
+    generateSample = function(baseline, group, stage2_assign = NULL) {
+      # generate samples following different normal distribution in 2 stages
+      assert_that(
+        is.numeric(baseline),
+        length(baseline) == 1,
+        (group == "drug" || group == "responder" || group == "nonresponder")
+      )
+      
+      # stage 1
+      if (group == "drug") {
+        self$mu1 <- private$m.meanFunction(private$m.b1, baseline)
+        self$sigma1 <- 49
+      } else if (group == "responder") {
+        self$mu1 <- private$m.meanFunction(private$m.b3, baseline)
+        self$sigma1 <- 4
+      } else {
+        self$mu1 <- private$m.meanFunction(private$m.b5, baseline)
+        self$sigma1 <- 4
+      }
+      y1 <- rnorm(1, self$mu1, sqrt(self$sigma1))
+      
+      # stage 2
+      if (group == "drug") {
+        self$mu2 <- private$m.meanFunction(private$m.b2, y1)
+        self$sigma2 <- 49
+      } else if (group == "responder") {
+        assert_that(stage2_assign == 0 | stage2_assign == 1)
+        self$mu2 <- private$m.meanFunction(private$m.b4, y1, assign = stage2_assign)
+        self$sigma2 <- 4
+      } else {
+        assert_that(stage2_assign == 0 | stage2_assign == 1)
+        self$mu2 <- private$m.meanFunction(private$m.b6, y1, assign = stage2_assign)
+        self$sigma2 <- 4
+      }
+      y2 <- rnorm(1, self$mu2, sqrt(self$sigma2))
+      
+      return (c(y1, y2))
+    }
+  ),
+  private = list(
+    # Drug parameters
+    m.b1 = NULL,
+    m.b2 = NULL,
+    # Placebo responders parameters
+    m.b3 = NULL,
+    m.b4 = NULL,
+    # Placebo nonresponders parameters
+    m.b5 = NULL,
+    m.b6 = NULL,
+    m.meanFunction = function(b, y, assign = NULL) {
+      if (length(b) == 2) {
+        return (b[1] + b[2]*y)
+      } else {
+        return (b[1] + b[2]*y + b[3]*assign)
+      }
+    }
+  )
+)
---
+++ main.R
@@ -0,0 +1,74 @@
+source("./class/NormalGenerativeModel.R")
+
+seed <- 101
+patient_size <- 10000
+pi <- 0.3 # placebo response probability
+drug_assign <- 0.4
+
+drug_size <- patient_size*drug_assign
+responder_size <- patient_size*(1 - drug_assign)*pi
+nonresponder_size <- patient_size*(1 - drug_assign)*(1 - pi)
+
+# baseline parameter
+mu0 <- 31
+sigma0 <- 25
+
+set.seed(seed = seed)
+
+# generate patients data
+y01 <- NULL
+group <- NULL
+stage2_assign <- NULL
+
+for (i in 1:drug_size) {
+  y01 <- append(y01, rnorm(1, mean = mu0, sd = sqrt(sigma0)))
+  group <- append(group, "drug")
+  stage2_assign <- append(stage2_assign, 0)
+}
+
+for (i in 1:responder_size) {
+  y01 <- append(y01, rnorm(1, mean = mu0, sd = sqrt(sigma0)))
+  group <- append(group, "responder")
+  if (i <= responder_size*drug_assign) {
+    stage2_assign <- append(stage2_assign, 1)
+  } else {
+    stage2_assign <- append(stage2_assign, 0)
+  }
+}
+
+for (i in 1:nonresponder_size) {
+  y01 <- append(y01, rnorm(1, mean = mu0, sd = sqrt(sigma0)))
+  group <- append(group, "nonresponder")
+  if (i <= nonresponder_size*drug_assign) {
+    stage2_assign <- append(stage2_assign, 1)
+  } else {
+    stage2_assign <- append(stage2_assign, 0)
+  }
+}
+
+# model parameter
+b1 <- c(1.5, 1.2)
+b2 <- c(1.5, 1.2)
+b3 <- c(0.5, 1.0)
+b4 <- c(0.5, 1.0, 2.5)
+b5 <- c(0.5, 1.2)
+b6 <- c(0.5, 1.2, 2.5)
+
+GenerativeModel <- NormalGenerativeModel$new(
+  b1=b1,
+  b2=b2,
+  b3=b3,
+  b4=b4,
+  b5=b5,
+  b6=b6
+)
+
+res <- mapply(GenerativeModel$generateSample, y01, group, stage2_assign)
+
+patient_data_baseline <- data.frame(
+  y01 = y01,
+  y1 = res[1,],
+  y2 = res[2,],
+  group = group,
+  stage2_assign = stage2_assign
+)
